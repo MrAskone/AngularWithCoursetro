f8a0d86f4d1bc78820784890b3dc188ed11de83b
MrAskone <antoine.sako.kone@gmail.com>
2018-03-13 18:35:01 +0100

    keyframes issues, up to the last bit!

--- components101/src/app/my-new-component/my-new-component.component.ts
+++ components101/src/app/my-new-component/my-new-component.component.ts
@@ -1,38 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { log } from 'util';
+import { DataService } from '../data.service'
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations'
+
 
 @Component({
   selector: 'app-my-new-component',
   // templateUrl: './my-new-component.component.html',
   template: `
 
-  <h1 [ngClass]="titleClasses">Hello!</h1>
-
-  `,
+  <p [@myAwesomeAnimation]='state' (click)="animateMe()" >I will animate!</p>
+  `,  // (mouseenter)="enlargeMe()" (mouseleave)="squeezeMe()"
   styles: [`
-  
-  h1 {
-    text-decoration:underline;
-  }
+    p {
+      width:200px;
+      background:lightgray;
+      margin: 100px auto;
+      text-align:center;
+      padding:20px;
+      font-size:1.5em;
+    }
+  `],
+  animations: [
+    trigger('myAwesomeAnimation', [
+      state('small', style({
+        transform: 'scale(1)',
+      })),
+      state('large', style({
+        transform: 'scale(1.2)',
+      })),
+      transition('small <=> large', animate('300ms ease-in', keyframes([
+        style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+        style({opacity: 1, transform: 'translateY(35px)', offset: 0.5}),
+        style({opacity: 1, transform: 'translateY(1)', offset: 1}),
+      ]))),
+    ]),
 
-  .red-title {
-    color: red;
-  }
-
-  .large-title {
-    font-size: 4em;
-  }
-
-  `]
+  ]
 })
 export class MyNewComponentComponent{
 
-  // constructor() { }
-  titleClass = false
-  titleClasses = {
-    'red-title' : true,
-    'large-title' : true
+  state:string = 'small';
+
+  constructor(private dataService:DataService) {
+
   }
 
+  animateMe() {
+    this.state = (this.state === 'small' ? 'large' : 'small');
+  };
+
+  // enlargeMe() {
+  //   this.state = 'large';
+  // }
+
+  // squeezeMe() {
+  //   this.state = 'small';
+  // }
 
 }
